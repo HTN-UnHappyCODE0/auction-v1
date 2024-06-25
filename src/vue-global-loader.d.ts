4f05f4547b0b04b0dcfcd0dc5dd639d07ba6f4b6
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-06-25 16:18:41 +0700

    update

---
+++ src/vue-global-loader.d.ts
@@ -0,0 +1,12 @@
+// src/types/vue-global-loader.d.ts
+declare module 'vue-global-loader/CircleSpinner.vue' {
+	import {DefineComponent} from 'vue';
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
+}
+// src/types/vue-global-loader.d.ts
+declare module 'vue-global-loader/GlobalLoader.vue' {
+	import {DefineComponent} from 'vue';
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
+}
